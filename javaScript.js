f8ee400dabea29c6eaa849acf4d7331e34736ae1
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-01-30 22:37:34 +0600

    updated

--- javaScript.js
+++ javaScript.js
@@ -38,10 +38,17 @@ function addTicket(InputValueId, price) {
 function removeTicket(InputValueId, price) {
     const inputValueString = document.getElementById(InputValueId).value;
     const inputValue = parseFloat(inputValueString);
-    const newInputValue = inputValue - 1;
-    document.getElementById(InputValueId).value = newInputValue;
-    //calc subtotal
-    calcSubtotal("subtotal", price, -1);
+
+    if (inputValue > 0) {
+        const newInputValue = inputValue - 1;
+        document.getElementById(InputValueId).value = newInputValue;
+        //calc subtotal
+        calcSubtotal("subtotal", price, -1);
+    }
+    else {
+        alert("Ticket amount can not be negative.");
+    }
+
 }
 
 function calcSubtotal(subtotalId, price, sign) {
@@ -51,5 +58,33 @@ function calcSubtotal(subtotalId, price, sign) {
     document.getElementById(subtotalId).innerText = newSubtotal;
 
     //calc vat
+    calcVat(newSubtotal, price, sign);
+
 
 }
+function calcVat(newSubtotal, price, sign) {
+    const vatString = document.getElementById("vat").innerText;
+    const vat = parseFloat(vatString);
+    const newVat = vat + (sign * price * 0.1);
+    document.getElementById("vat").innerText = newVat;
+
+    //calc total
+    calcTotal(newSubtotal, newVat);
+}
+
+function calcTotal(newSubtotal, newVat) {
+    const totalString = document.getElementById("total").innerText;
+    const total = parseFloat(totalString);
+    const newTotal = newSubtotal + newVat;
+    document.getElementById("total").innerText = newTotal;
+}
+
+//book now
+// login event handler
+const bookNowBtn = document.getElementById('bookNow');
+bookNowBtn.addEventListener("click", function () {
+    const loginArea = document.getElementById('booking-area');
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("confirmation-area");
+    transactionArea.style.display = "block";
+});
